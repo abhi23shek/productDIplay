a71bf956591b04eb51e9dac50cc3e1f4a9fd744b
saurav-kumar-2609 <verybuddha2222@gmail.com>
2025-01-10 19:57:48 +0530

    navbar changes for screen type

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -15,46 +15,32 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
       <div className="container-fluid">
         {/* Company Logo */}
-        <div className="navbar-logo mx-auto" onClick={() => navigate("/")}>
-          <img
-            src={require("./image/ShivCollection logo.png")} // Update with your actual logo path
-            alt="Company Logo"
-            className="logo-img"
-          />
+        <div className="leftside_navbar">
+          <div className="navbar-logo mx-auto" onClick={() => navigate("/")}>
+            <img
+              src={require("./image/ShivCollection logo.png")} // Update with your actual logo path
+              alt="Company Logo"
+              className="logo-img"
+            />
+          </div>
+          <button
+            className={`navbar-brand text-dark rounded-pill px-4 shiny-text ${
+              isActive("/") ? "border border-dark" : ""
+            } home-btn`}
+            onClick={() => navigate("/")}
+          >
+            Home
+          </button>
+          <button
+            className={`nav-link navbar-brand text-dark rounded-pill px-4 shiny-text ${
+              isActive("/Contact-us") ? "border border-dark" : ""
+            } about-btn`}
+            onClick={() => navigate("/Contact-us")}
+          >
+            Contact Us
+          </button>
         </div>
-        <button
-          className={`navbar-brand text-dark rounded-pill px-4 shiny-text ${
-            isActive("/") ? "border border-dark" : ""
-          } home-btn`}
-          onClick={() => navigate("/")}
-        >
-          Home
-        </button>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <button
-                className={`nav-link navbar-brand text-dark rounded-pill px-4 shiny-text ${
-                  isActive("/Contact-us") ? "border border-dark" : ""
-                } about-btn`}
-                onClick={() => navigate("/Contact-us")}
-              >
-                Contact Us
-              </button>
-            </li>
-          </ul>
-
+        <div className="rightside_navbar">
           <div className="d-flex">
             {auth ? (
               <button
