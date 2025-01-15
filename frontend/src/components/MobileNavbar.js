460dc6048e232145c33911aaec0b53f87278c12c
saurav-kumar-2609 <verybuddha2222@gmail.com>
2025-01-15 20:49:21 +0530

    minor change

--- frontend/src/components/MobileNavbar.js
+++ frontend/src/components/MobileNavbar.js
@@ -20,7 +20,7 @@ const MobileNavbar = () => {
             className="company-logo"
           />
         </div>
-        <div className="companyname">shivcollection</div>
+        <div className="companyname">shiv collection</div>
 
         {/* Hamburger Menu */}
         <div className="menu-toggle">
