395aa6aa89c313a06813c97df95909ad948aa651
saurav-kumar-2609 <verybuddha2222@gmail.com>
2025-02-11 00:02:52 +0530

    adminnavbar

--- frontend2/src/components/AdminAddProduct.js
+++ frontend2/src/components/AdminAddProduct.js
@@ -142,7 +142,7 @@ const AdminAddProduct = ({ categories }) => {
 
   return (
     <div className="container my-4">
-      <div className="card shadow-sm">
+      <div className="Admin-card shadow-sm">
         <div className="card-body">
           <h2 className="card-title text-center mb-4">Add New Product</h2>
           {error && <p className="alert alert-danger">{error}</p>}
